7b6edc0e1c865eef0182a6a6e31d2419d8f79a13
Michael Droettboom <mdboom@gmail.com>
2022-10-12 08:35:59 -0400

    Fix aggregation script

--- .github/workflows/combine-benchmarks.py
+++ .github/workflows/combine-benchmarks.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pyperf
 
 for root in ("head", "base"):
-    files = list(Path().glob(f"{root}-*.json"))
+    files = list(Path().glob(f"**/{root}-*.json"))
     suite = pyperf.BenchmarkSuite.load(str(files[0]))
     for file in files[1:]:
         subsuite = pyperf.BenchmarkSuite.load(str(file))
